a87c4b1f1e4ce71a3ae5e71221553e689899a328
K1977 <K1977@TIET-327-WS-21>
2017-02-15 16:35:47 +0200

    Tehtävä1 tehty

--- demo05/Program.cs
+++ demo05/Program.cs
@@ -10,6 +10,51 @@ namespace demo05
     {
         static void Main(string[] args)
         {
+            //create Persons(collection)
+            Persons myFriends = new Persons();
+
+            //create a few Person objects
+            Person person1 = new Person
+            {
+                Firstname = "Sika",
+                Lastname = "Rykä",
+                SocialSecurityNumber = "010101-123A"
+            };
+            Person person2 = new Person
+            {
+                Firstname = "Teppo",
+                Lastname = "Matikainen",
+                SocialSecurityNumber = "030303-222B"
+            };
+
+            //add person objects to persons (myfriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+
+            //get one more person
+            Person person3 = myFriends.GetPerson(0);
+            if (person3 != null)
+            {
+                Console.WriteLine(person3.ToString());
+            } else
+            {
+                Console.WriteLine("Can't find that person.");
+            }
+            //prtin collection
+            myFriends.PrintData();
+
+            //search person
+            string sotu = "030303-222B";
+            Console.WriteLine("Etsi henkilö, jolla sotu: " + sotu);
+            Person person4 = myFriends.FindPerson(sotu);
+            if (person4 != null)
+            {
+                Console.WriteLine("Haulla löytyi seuraava henkilö:");
+                Console.WriteLine(person4.ToString());
+            }else
+            {
+                Console.WriteLine("Kyseistä henkilöä ei löydy valitulla sotulla");
+            }
         }
     }
 }
