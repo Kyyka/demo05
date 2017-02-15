9fe5e2a5a303fbee5ae7e7c70fc9d69e5297b2ed
Eetu <Eetu@DESKTOP-FM7QUSI>
2017-02-15 19:30:46 +0200

    teht03 tehty

---
+++ teht03/List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht03
+{
+    class List
+    {
+        public string Developer { get; set; }
+        public List<Game> Games = new List<Game>();
+
+        public void AddGame (string name, int year)
+        {
+            Games.Add(new Game { Name = name, Year = year });
+        }
+
+        public override string ToString()
+        {
+            string DevGames = "";
+            foreach(Game item in Games)
+            {
+                DevGames = DevGames + item.ToString() + "\n";
+            }
+            return "------------------------\nDeveloper: " + Developer + "\n-----List of games:-----\n" + DevGames;
+        }
+    }
+}
---
+++ teht03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht03
+{
+    class Program
+    {
+        //Listataan pelikehittäjiä ja heidän pelejään.
+        static void Main(string[] args)
+        {
+            List dev = new teht03.List();
+            dev.Developer = "Bethesda Softworks";
+            dev.AddGame("The Elder Scrolls", 1994);
+            dev.AddGame("Terminator", 1995);
+            Console.WriteLine(dev.ToString());
+
+            List dev2 = new teht03.List();
+            dev2.Developer = "id Software";
+            dev2.AddGame("Quake", 1996);
+            dev2.AddGame("Quake II", 1997);
+            dev2.AddGame("Quake III Arena", 1999);
+            Console.Write(dev2.ToString());
+        }
+    }
+}
